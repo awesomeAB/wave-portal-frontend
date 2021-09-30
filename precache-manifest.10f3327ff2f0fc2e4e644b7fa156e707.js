2dc2bfe5c2ec03179ef09481f5a9c2fb5f68c63f
Abhishek Bisht <abhisinghbisht04@gmail.com>
2021-09-30 13:53:34 +0100

    Updates

---
+++ precache-manifest.10f3327ff2f0fc2e4e644b7fa156e707.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "27569e3d64512ac4180904b170cfbfe4",
+    "url": "/wave-portal-frontend/index.html"
+  },
+  {
+    "revision": "4be54e252fb4b7e2929c",
+    "url": "/wave-portal-frontend/static/css/main.7cf057bb.chunk.css"
+  },
+  {
+    "revision": "daec4ecd52cad16876a9",
+    "url": "/wave-portal-frontend/static/js/2.8cbbd9a7.chunk.js"
+  },
+  {
+    "revision": "4be54e252fb4b7e2929c",
+    "url": "/wave-portal-frontend/static/js/main.b5426f7c.chunk.js"
+  },
+  {
+    "revision": "ae7be694d3202a5b584f",
+    "url": "/wave-portal-frontend/static/js/runtime-main.14ea7b59.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/wave-portal-frontend/precache-manifest.10f3327ff2f0fc2e4e644b7fa156e707.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/wave-portal-frontend/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
